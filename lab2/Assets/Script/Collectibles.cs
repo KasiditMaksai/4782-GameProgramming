142e6152b5546c7f3682304c6e5e0aa52b0b94ba
KasiditMaksai <kasidit.m@kkumail.com>
2022-09-13 22:41:38 +0700

    Add file

--- lab2/Assets/Script/Collectibles.cs
+++ lab2/Assets/Script/Collectibles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,4 +7,22 @@ using UnityEngine.InputSystem;
 public class Collectibles : MonoBehaviour
 {
    public CollectibleType color;
+
+   void Start()
+   {
+      if (TryGetComponent<CollectibleRandom>(out CollectibleRandom collectibleRandomize))
+      {
+         color = collectibleRandomize.color;
+      }
+   }
+
+   CollectibleType getColor()
+   {
+      return this.color;
+   }
+
+   void setColor(CollectibleType color)
+   {
+      this.color = color;
+   }
 }
