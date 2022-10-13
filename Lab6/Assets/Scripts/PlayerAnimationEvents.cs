c18508f38b8404f764f618a8d6aa5d74b184e3ec
KasiditMaksai <kasidit.m@kkumail.com>
2022-10-13 18:03:47 +0700

    lab6

---
+++ Lab6/Assets/Scripts/PlayerAnimationEvents.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+    [SerializeField] private PlayerAudioController audioController;
+    
+
+    public void PlayWalkSound()
+    {
+        audioController.PlayWalk();
+        
+    }
+}
